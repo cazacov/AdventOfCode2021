ad684958039c08bbbefbb2a0608204fc0f3be3ce
Victor Cazacov <cazacov.git@gmail.com>
2021-12-04 21:11:34 +0100

    Day 4, puzzles 1 and 2

---
+++ CSharp/Day04/Board.cs
@@ -0,0 +1,79 @@
+﻿internal class Board
+{
+    private bool[,] AreMarked = new bool[5,5];
+    private int [,] Numbers = new int[5,5];
+
+    public Board(List<int> numbers)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Numbers[i, j] = numbers[i * 5 + j];
+            }
+        }
+    }
+
+    internal void Mark(int n)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (Numbers[i, j] == n)
+                {
+                    AreMarked[i, j] = true;
+                }
+            }
+        }
+    }
+
+    internal bool HasWon()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            bool row = true;
+            bool column = true;
+            for (int j = 0; j < 5; j++)
+            {
+                row &= AreMarked[i, j];
+                column &= AreMarked[j, i];
+            }
+            if (row || column)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    internal int Score
+    {
+        get
+        {
+            var score = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (!AreMarked[i, j])
+                    {
+                        score += Numbers[i, j];
+                    }
+                }
+            }
+            return score;
+        }
+    }
+
+    internal void Reset()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                AreMarked[i, j] = false;
+            }
+        }
+    }
+}
---
+++ CSharp/Day04/Program.cs
@@ -0,0 +1,77 @@
+﻿using AocHelpers;
+
+Console.WriteLine("Advent of Code 2021, day 4");
+var input = Helpers.ReadStrings("input.txt");
+
+var numbers = input[0].Split(",").ToList().ConvertAll(x => Int32.Parse(x)).ToList();
+var boards = ParseBoards(input.Skip(2));
+
+
+Puzzle1(boards, numbers);
+
+foreach(var board in boards)
+{
+    board.Reset();
+}
+
+Puzzle2(boards, numbers);
+
+
+
+
+
+void Puzzle1(List<Board> boards, List<int> numbers)
+{
+    foreach (var n in numbers)
+    {
+        foreach (var board in boards)
+        {
+            board.Mark(n);
+            if (board.HasWon())
+            {
+                Console.WriteLine(board.Score * n);
+                return;
+            }
+        }
+    }
+}
+
+void Puzzle2(List<Board> boards, List<int> numbers)
+{
+    var set = new HashSet<Board>(boards);
+    foreach (var n in numbers)
+    {
+        foreach (var board in boards)
+        {
+            board.Mark(n);
+            if (board.HasWon())
+            {
+                set.Remove(board);
+                if (set.Count == 0)
+                {
+                    Console.WriteLine(board.Score * n); 
+                    return;
+                }
+            }
+        }
+    }
+}
+
+List<Board> ParseBoards(IEnumerable<string> enumerable)
+{
+    var result = new List<Board>();
+    var numbers = new List<int>();    
+    foreach (var str in enumerable) { 
+        if (String.IsNullOrWhiteSpace(str))
+        {
+            result.Add(new Board(numbers));
+            numbers.Clear();
+        }
+        numbers.AddRange(str.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList().ConvertAll(Int32.Parse));
+    }
+    if (numbers.Count > 0)
+    {
+        result.Add(new Board(numbers));
+    }
+    return result;
+}
