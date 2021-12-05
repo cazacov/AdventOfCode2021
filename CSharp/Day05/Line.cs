22662d352246c45ed87db3175ccd3f9c2c5089ce
Victor Cazacov <cazacov.git@gmail.com>
2021-12-05 11:19:00 +0100

    Day 5, puzzles 1 and 2

---
+++ CSharp/Day05/Line.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+[DebuggerDisplay("{X1},{Y1}->{X2},{Y2}")]
+internal class Line
+{
+    public int X1;
+    public int Y1;
+
+    public int X2;
+    public int Y2;
+
+    public Line(string str)
+    {
+        var pattern = @"(\d+),(\d+) -> (\d+),(\d+)";
+        var match = Regex.Match(str, pattern);
+        if (!match.Success) { 
+            throw new ArgumentException("Invalid input");
+        }
+        X1 = Int32.Parse(match.Groups[1].Value);
+        Y1 = Int32.Parse(match.Groups[2].Value);
+        X2 = Int32.Parse(match.Groups[3].Value);
+        Y2 = Int32.Parse(match.Groups[4].Value);
+    }
+
+    internal void Apply(ref int[,] map)
+    {
+        var dirx = 0;
+        if (X1 < X2) { dirx = 1; }
+        if (X1 > X2) { dirx = -1; }
+
+        var diry = 0;
+        if (Y1 < Y2) { diry = 1; }
+        if (Y1 > Y2) { diry = -1; }
+
+        var x = X1 - dirx;
+        var y = Y1 - diry;
+        do
+        {
+            x += dirx;
+            y += diry;
+            map[x, y] = map[x, y] + 1;
+        } while (x != X2 || y != Y2);
+    }
+
+    /// <summary>
+    /// The line is horizontal or vertical
+    /// </summary>
+    public bool IsHorV
+    {
+        get
+        {
+            return X1 == X2 || Y1 == Y2;
+        }
+    }
+}
---
+++ CSharp/Day05/Program.cs
@@ -0,0 +1,48 @@
+﻿using AocHelpers;
+
+Console.WriteLine("Advent of Code 2021, day 5");
+var lines = Helpers.ReadStrings("input.txt").ConvertAll(x => new Line(x));
+
+
+Puzzle1(lines);
+Puzzle2(lines);
+
+void Puzzle1(List<Line> lines)
+{
+    var map = new int[1000, 1000];
+    foreach (var line in lines)
+    {
+        if (!line.IsHorV)
+        {
+            continue;
+        }
+        line.Apply(ref map);
+    }
+    Console.WriteLine(CountDangerousPlaces(map));
+}
+
+void Puzzle2(List<Line> lines)
+{
+    var map = new int[1000, 1000];
+    foreach (var line in lines)
+    {
+        line.Apply(ref map);
+    }
+    Console.WriteLine(CountDangerousPlaces(map));
+}
+
+int CountDangerousPlaces(int[,] map)
+{
+    var count = 0;
+    for (int i = 0; i < 1000; i++)
+    {
+        for (int j = 0; j < 1000; j++)
+        {
+            if (map[i, j] > 1)
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
