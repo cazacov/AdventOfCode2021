686983787cb244db259d36a03a2c83faa82bc971
Victor Cazacov <cazacov.git@gmail.com>
2021-12-09 08:44:37 +0100

    Day 9, puzzles 1 and 2

---
+++ CSharp/Day09/Point.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+[DebuggerDisplay("{X},{Y}")]
+internal class Point
+{
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
+    public Point(int x, int y)
+    {
+        X = x; Y = y;
+    }
+
+    public List<Point> Neighbours
+    {
+        get
+        {
+            var result = new List<Point>();
+            for (int y = -1; y <= 1; y++)
+            {
+                for (int x = -1; x <= 1; x++)
+                {
+                    if (Y + y < 0 || Y + y >= 100) continue;
+                    if (X + x < 0 || X + x >= 100) continue;
+                    if (y == 0 && x == 0) continue;
+                    if (y * x != 0) continue;
+                    result.Add(new Point(X + x, Y + y));
+                }
+            }
+            return result;
+        }
+    }
+
+    protected bool Equals(Point other)
+    {
+        return X == other.X && Y == other.Y;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Point)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y);
+    }
+}
+
---
+++ CSharp/Day09/Program.cs
@@ -0,0 +1,89 @@
+﻿using AocHelpers;
+
+Console.WriteLine("Advent of Code 2021, day 9");
+var map = ReadHeightMap("input.txt");
+
+var lowPoints = Puzzle1(map);
+Puzzle2(map, lowPoints);
+
+List<Point> Puzzle1(Dictionary<Point, int> map)
+{
+    var result = new List<Point>();
+    int risk = 0;
+    for (int i = 0;  i < 100; i++)
+    {
+        for (int j = 0; j < 100; j++)
+        {
+            var point = new Point(j, i);
+            var depth = map[point];
+            if (point.Neighbours.All(p => map[p] > depth))
+            {
+                risk += 1 + map[point];
+                result.Add(point);
+            }
+        }
+    }
+    Console.WriteLine(risk);
+    return result;
+}
+
+void Puzzle2(Dictionary<Point, int> map, List<Point> lowPoints)
+{
+    var basinSizes = new List<int>();
+
+    foreach (var point in lowPoints)
+    {
+        int basinSize = 0;
+        var current = point;
+        var todo = new HashSet<Point>();
+        var inspected = new HashSet<Point>();
+
+        do
+        {
+            inspected.Add(current);
+            basinSize++;
+            foreach (var next in current.Neighbours)
+            {
+                if (inspected.Contains(next))
+                {
+                    continue;
+                }  
+                if  (map[next] == 9)
+                {
+                    continue;
+                }
+                todo.Add(next);
+            }
+            todo.Remove(current);
+            if (todo.Count > 0)
+            {
+                current = todo.First();
+            }
+            else
+            {
+                current = null;
+            }
+        } while (current != null);
+        basinSizes.Add(basinSize);
+    }
+    Console.WriteLine(basinSizes
+                        .OrderByDescending(x => x)
+                        .Take(3)
+                        .Aggregate(1, (acc, item) => acc * item));
+}
+
+Dictionary<Point,int> ReadHeightMap(string fileName)
+{
+    var result = new Dictionary<Point, int>();
+
+    var lines = System.IO.File.ReadAllLines(fileName);
+    for (int i = 0; i < 100; i++)
+    {
+        var str = lines[i];
+        for (int j = 0; j < 100; j++)
+        {
+            result[new Point(j, i)] = Int32.Parse(str[j].ToString());
+        }
+    }
+    return result;
+}
